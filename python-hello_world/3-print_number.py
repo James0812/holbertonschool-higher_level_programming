aa335cc272fc9f976fa0167f52c3bb49ef2af80e
James0812 <james.roussel008@gmail.com>
2026-01-12 13:42:52 +0100

    add the new code in the directory

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-number_pos = 333
-print(f"{number_pos} Battery street")
-
-number_neg = -98
-print(f"{number_neg} Battery street")
+number = 98
+print("{:d} Battery street".format(number))
