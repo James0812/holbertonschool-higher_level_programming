d232c8a1b8686a17efa9e7d76fb65cd83bb17bfc
James0812 <james.roussel008@gmail.com>
2026-01-21 10:15:42 +0100

    add the code

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
-"""Module that defines a function to print a full name."""
+"""Module that prints 'My name is <first_name> <last_name>'"""
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    Prints My name is <first_name> <last_name>.
-
-    Args:
-        first_name (str): first name
-        last_name (str, optional): last name. Defaults to "".
-
-    Raises:
-        TypeError: if first_name or last_name is not a string
-    """
+    """Prints the full name with proper validation"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
-    print(f"My name is {first_name} {last_name}".rstrip())
+    print("My name is {} {}".format(first_name, last_name).rstrip() + " ")
