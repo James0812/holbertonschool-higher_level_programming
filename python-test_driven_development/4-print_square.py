de28a25c846bba64a45bf3caca86b2dcb9619b0d
James0812 <james.roussel008@gmail.com>
2026-01-21 10:34:36 +0100

    add the code

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""Module 4-print_square: prints a square with the character #"""
+"""Module that prints a square with the character #."""
+
 
 def print_square(size):
     """
-    Prints a square of the given size using the '#' character.
+    Prints a square with the character #.
 
     Args:
-        size (int): the length of the square's sides
+        size (int): size of the square
 
     Raises:
         TypeError: if size is not an integer
         ValueError: if size is less than 0
     """
-    # Vérification du type
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    
-    # Vérification de la valeur
     if size < 0:
         raise ValueError("size must be >= 0")
-
-    # Impression de la ligne de la taille demandée
     for _ in range(size):
         print("#" * size)
