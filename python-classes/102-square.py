ca1b5a3d839f9c7c18c2f4824dbf4fce7c0daaae
James0812 <james.roussel008@gmail.com>
2026-01-28 10:26:15 +0100

    add the code in the directory

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -6,17 +6,14 @@ class Square:
     """Represents a square."""
 
     def __init__(self, size=0):
-        """Initialize a Square."""
         self.size = size
 
     @property
     def size(self):
-        """Retrieve the size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size with validation."""
         if not isinstance(value, (int, float)):
             raise TypeError("size must be a number")
         if value < 0:
@@ -24,42 +21,24 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Return the area of the square."""
         return self.__size ** 2
 
     def __eq__(self, other):
-        """Equal to."""
-        if not isinstance(other, Square):
-            return NotImplemented
         return self.area() == other.area()
 
     def __ne__(self, other):
-        """Not equal to."""
-        if not isinstance(other, Square):
-            return NotImplemented
         return self.area() != other.area()
 
     def __lt__(self, other):
-        """Less than."""
-        if not isinstance(other, Square):
-            return NotImplemented
         return self.area() < other.area()
 
     def __le__(self, other):
-        """Less than or equal to."""
-        if not isinstance(other, Square):
-            return NotImplemented
         return self.area() <= other.area()
 
     def __gt__(self, other):
-        """Greater than."""
-        if not isinstance(other, Square):
-            return NotImplemented
         return self.area() > other.area()
 
     def __ge__(self, other):
-        """Greater than or equal to."""
-        if not isinstance(other, Square):
-            return NotImplemented
         return self.area() >= other.area()
+
 
