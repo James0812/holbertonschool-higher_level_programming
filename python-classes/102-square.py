234e026c7b79112d661bcdafb1d5eda325cc527a
James0812 <james.roussel008@gmail.com>
2026-01-28 10:27:12 +0100

    add the code in the directory

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -10,10 +10,12 @@ class Square:
 
     @property
     def size(self):
+        """Retrieve the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set the size of the square."""
         if not isinstance(value, (int, float)):
             raise TypeError("size must be a number")
         if value < 0:
@@ -21,24 +23,36 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Return the area of the square."""
         return self.__size ** 2
 
     def __eq__(self, other):
+        if not isinstance(other, Square):
+            return NotImplemented
         return self.area() == other.area()
 
     def __ne__(self, other):
+        if not isinstance(other, Square):
+            return NotImplemented
         return self.area() != other.area()
 
     def __lt__(self, other):
+        if not isinstance(other, Square):
+            return NotImplemented
         return self.area() < other.area()
 
     def __le__(self, other):
+        if not isinstance(other, Square):
+            return NotImplemented
         return self.area() <= other.area()
 
     def __gt__(self, other):
+        if not isinstance(other, Square):
+            return NotImplemented
         return self.area() > other.area()
 
     def __ge__(self, other):
+        if not isinstance(other, Square):
+            return NotImplemented
         return self.area() >= other.area()
-
 
