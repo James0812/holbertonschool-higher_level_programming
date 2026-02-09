e5ca44048debbffb1318484ac2bb8f6ba3e55863
James0812 <james.roussel008@gmail.com>
2026-02-09 09:54:31 +0100

    add the code in the file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module for reading and printing a text file."""
+
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
