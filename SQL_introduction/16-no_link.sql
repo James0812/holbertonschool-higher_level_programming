5fa72ba51ad6510887b12a96aeaadb7a08925bfd
James0812 <james.roussel008@gmail.com>
2026-03-02 10:51:04 +0100

    add the code in the file

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- List all records of second_table with a non-empty name, ordered by descending score
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL AND name != ''
+ORDER BY score DESC;
