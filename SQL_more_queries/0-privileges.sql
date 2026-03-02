e9d15c922b81d2f5778dbe413f921f9cf8b77c89
James0812 <james.roussel008@gmail.com>
2026-03-02 11:01:12 +0100

    add the code in the directory

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,13 +1,3 @@
--- List all privileges of user_0d_1 and user_0d_2
-
--- Create the users if they do not exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant privileges so we can show something (optionnel si le checker l’exige)
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Show privileges
+-- List all privileges of MySQL users user_0d_1 and user_0d2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
