394bd5d5e27c2f0170afa956f37f25b4b0aa4ffa
James0812 <james.roussel008@gmail.com>
2026-03-02 11:31:38 +0100

    add the code in the directory

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
--- 11-genre_id_all_shows.sql
--- List all shows and their genre_id (NULL if no genre)
+-- List all shows contained in database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
