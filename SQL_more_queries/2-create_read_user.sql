85aa56817a33720fa0c8a0f117052b26f8d910bb
James0812 <james.roussel008@gmail.com>
2026-03-02 11:02:43 +0100

    add the code in the directory

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,13 @@
+-- Create the database hbtn_0d_2 and the user user_0d_2
+
+-- Create database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant only SELECT privilege on hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Apply privileges
+FLUSH PRIVILEGES;
