954b7ba544a31ce795ea7bf236e17b5ddcbb0c2e
James0812 <james.roussel008@gmail.com>
2026-03-02 10:59:03 +0100

    add the code in the directory

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,13 +1,2 @@
--- Show all privileges of user_0d_1 and user_0d_2
-
--- Create the users if they do not exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant privileges (optional if needed)
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- List privileges
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Create the database hbtn_0c_0 if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
