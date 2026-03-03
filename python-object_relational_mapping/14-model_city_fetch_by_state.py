ea2d9c5bc56173173f3837b44b4fdccfc7be86c0
James0812 <james.roussel008@gmail.com>
2026-03-03 13:53:10 +0100

    add the code in the file

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
 """
-script that prints all City objects from the database
-hbtn_0e_14_usa sorted by cities.id
+Contains a script that prints all City objects from the database hbtn_0e_14_usa
 """
 
+
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 from model_city import City
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
-def fetch_cities_by_state():
-    """Fetch and print all cities with their state"""
+def print_cities():
+    """
+    Prints all City objects from the database hbtn_0e_14_usa.
+    """
+
     arg = sys.argv
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(arg[1], arg[2], arg[3]),
-        pool_pre_ping=True
-    )
+    url_base = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    db_url = url_base.format(arg[1], arg[2], arg[3])
+    engine = create_engine(db_url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Join City and State to print <state name>: (<city id>) <city name>
-    results = session.query(City, State).join(State, City.state_id == State.id).order_by(City.id).all()
-    for city, state in results:
-        print(f"{state.name}: ({city.id}) {city.name}")
-
-    session.close()
+    search = session.query(City, State).filter(City.state_id == State.id)
+    for c, s in search:
+        print(
+            s.name,
+            ': ',
+            '(',
+            c.id,
+            ') ',
+            c.name,
+            sep=""
+        )
 
 
 if __name__ == "__main__":
-    fetch_cities_by_state()
+    print_cities()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,16 +1,39 @@
 #!/usr/bin/python3
 """
-module that defines the City class linked to the 'cities' table
+Contains the class definition of City.
 """
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+
+import sys
+from sqlalchemy import (
+    Integer,
+    String,
+    Column,
+    ForeignKey
+)
+from model_state import State
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
 
 
 class City(Base):
-    """City class linked to the 'cities' table"""
-    __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    """The City class declaration"""
+    __tablename__ = "cities"
+    id = Column(
+        Integer,
+        autoincrement=True,
+        unique=True,
+        nullable=False,
+        primary_key=True
+    )
+    name = Column(
+        String(128),
+        nullable=False
+    )
+    state_id = Column(
+        Integer,
+        ForeignKey(State.id),
+        nullable=False
+    )
