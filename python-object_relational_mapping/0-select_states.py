8bd6e8d4e1b95f55489f28368facfb1ecfee5d7b
James0812 <james.roussel008@gmail.com>
2026-03-03 09:46:45 +0100

    add the code in the file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
-import sys
-import MySQLdb
+""" ℹ️
+    lists all states from the database hbtn_0e_0_usa:
+"""
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+
+    import sys
+    import MySQLdb
+
+    user_name = sys.argv[1]
+    passw = sys.argv[2]
+    data_base = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user_name, passwd=passw, db=data_base)
+    cur = db.cursor()
+    cur.execute("""SELECT *
+                FROM
+                states""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
