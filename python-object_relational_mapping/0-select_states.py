fa4921b5ee621a54986617496873b6652a34ff1c
James0812 <james.roussel008@gmail.com>
2026-03-03 09:45:09 +0100

    add the code in the file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,16 +3,17 @@
 Lists all states from the database hbtn_0e_0_usa.
 """
 
-if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
+import MySQLdb
+import sys
 
+
+if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
     cursor = db.cursor()
