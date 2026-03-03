770fab40a953d695764467abca3a6c660f5e01a4
James0812 <james.roussel008@gmail.com>
2026-03-03 09:52:05 +0100

    add the code in the file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """
-Script that lists all states from a MySQL database ordered by id.
+Lists all states from the database hbtn_0e_0_usa.
 """
+
 
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
+    connection = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        charset="utf8"
     )
-
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-
-    for row in cursor.fetchall():
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
         print(row)
-
     cursor.close()
-    db.close()
+    connection.close()
