814ee169cb411d73071b5e35de47015a0356ca99
James0812 <james.roussel008@gmail.com>
2026-03-03 10:07:53 +0100

    add the code in the file

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,11 +9,13 @@ import sys
 
 
 if __name__ == "__main__":
+    # Arguments
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
+    # Connexion à MySQL
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -23,9 +25,8 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    # Utilisation de format() pour la requête SQL
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
-    cur.execute(query)
+    # Requête avec format() strict
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name))
     rows = cur.fetchall()
 
     for row in rows:
