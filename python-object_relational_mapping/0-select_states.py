3185d20168797ef98678292fa318ebc914efe9d4
James0812 <james.roussel008@gmail.com>
2026-03-03 09:42:18 +0100

    add the code in the file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
+"""
+Module name: 0-selected_states.
 
-
+Contains something.
+"""
 if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    db_name = argv[3]
+
     db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    rows = cur.fetchall()
-
-    for row in rows:
+        user=mysql_username,
+        passwd=mysql_password,
+        database=db_name,
+        host='localhost',
+        port=3306)
+    c = db.cursor()
+    c.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    for row in c.fetchall():
         print(row)
-
-    cur.close()
-    db.close()
