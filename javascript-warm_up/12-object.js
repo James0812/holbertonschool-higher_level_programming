bf044eecf6bc74b5ea452bde9154a5ab9865c19b
James0812 <james.roussel008@gmail.com>
2026-03-17 14:00:15 +0100

    Add the code

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-const numbers = [1, 5, 89, 4, 8];
+const value = 12;
+const numbers = [1, 5, value, 4, 8];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] === value) numbers[i] = 89;
+}
 console.log(numbers);
