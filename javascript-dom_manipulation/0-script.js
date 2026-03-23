94726bdacaf688d39fcf109a27d4d57c0100e9bd
James0812 <james.roussel008@gmail.com>
2026-03-23 09:55:37 +0100

    Add the code

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,8 @@
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector("header");
+  if (header) {
+    header.style.color = "#FF0000";
+  }
+});
+</script>
